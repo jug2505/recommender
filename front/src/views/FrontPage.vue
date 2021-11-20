735173d551b9cb09d8bf6b71cb248a7f6901cdf1
jug2505 <jonkuvshin@gmail.com>
2021-11-20 09:46:22 +0300

    feat: card for movie

--- front/src/views/FrontPage.vue
+++ front/src/views/FrontPage.vue
@@ -1,11 +1,15 @@
 <template>
-  <h1>FrontPage</h1>
+  <div>
+    <film-card cardImage="https://via.placeholder.com/315x140" title="title" genre="genre"/>
+  </div>
+
 </template>
 
 <script>
-
+import FilmCard from "../components/FilmCard";
 export default {
   name: "FrontPage",
+  components: {FilmCard},
   data () {
     return {
     }
